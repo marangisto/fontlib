a1367fd07332b636886f38b76b63322b219c780a
Bengt Marten Agren <marangisto@me.com>
2019-05-29 22:24:04 +0800

    Working SDL2 skeleton (Linux only).

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <SDL.h>
+#include <SDL2/SDL.h>
 
 #define SCREEN_WIDTH 240
 #define SCREEN_HEIGHT 240
@@ -12,5 +12,20 @@ int main()
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
         throw "could not initialize SD2";
+    if (!(window = SDL_CreateWindow
+        ( "Hello World!"
+        , SDL_WINDOWPOS_UNDEFINED
+        , SDL_WINDOWPOS_UNDEFINED
+	    , SCREEN_WIDTH
+        , SCREEN_HEIGHT
+        , SDL_WINDOW_SHOWN
+        )))
+        throw "could not create window";
+    surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(surface, 0, SDL_MapRGB(surface->format, 0xff, 0xff, 0x00));
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
