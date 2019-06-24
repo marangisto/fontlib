444246a7b322169459837bc3f8d9dff1f40c4e96
Bengt Marten Agren <marangisto@me.com>
2019-06-24 19:52:25 +0800

    Fixme change.

--- src/fontlib.cpp
+++ src/fontlib.cpp
@@ -8,7 +8,7 @@ const glyph_t *get_glyph(const font_t& font, char c)
     if (c >= font.first && c <= font.last)
         return &font.glyphs[c - font.first];
     else
-        return 0;   // FIXME: use maybe_t<T>?
+        return 0;   // FIXME: add box glyph
 }
 
 } // fontlib
