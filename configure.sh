9eb49eda85616dd60f61270bd6775f679b504fbf
Bengt Marten Agren <marangisto@me.com>
2020-10-24 14:29:26 +0800

    Added 3270 fonts.

--- configure.sh
+++ configure.sh
@@ -5,8 +5,11 @@
     cd ttf
     for ARCHIVE in *.zip
     do
-        unzip $ARCHIVE
+        unzip -o $ARCHIVE
     done
+
+    [ ! -d 3270 ] && mkdir 3270
+    mv 3270*.ttf 3270
 )
 
 FONTLIST=include/fontlist.h
@@ -16,12 +19,26 @@ echo "{" >> $FONTLIST
 
 find ttf -name '*.ttf' -print|while read TTF
 do
-    FONT=`basename "$TTF" .ttf`
-    echo $TTF $FONT
+    BASE=`basename "$TTF" .ttf`
+    case "${BASE}" in
+        "3270-Medium 500")
+            FONT=ibm3270m
+            ;;
+        "3270 Narrow 500")
+            FONT=ibm3270n
+            ;;
+        "3270 Semi-Narrow 500")
+            FONT=ibm3270s
+            ;;
+        cmu*)
+            FONT=${BASE}
+            ;;
+    esac
     for SIZE in 16 20 24 28 32 36 40 44 48
     do
         NAME=${FONT}_${SIZE}
         TARGET=src/${NAME}.cpp
+        echo ${FONT} ${SIZE}
         Fonterizer --name=$NAME --size=$SIZE "$TTF" > $TARGET
         echo "extern const font_t& ${FONT}_${SIZE};" >> $FONTLIST
     done
